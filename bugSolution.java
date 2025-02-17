9d6dd4cf53dc908f1f5d15c320d3ffd6e9599121
BugHunter-X <askzenai@gmail.com>
2025-02-17 09:04:04 +0100

    Add bugSolution.java file

---
+++ bugSolution.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 0;
+        try {
+            int k = i / j; 
+            System.out.println(k);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero.");
+        }
+    }
+}
